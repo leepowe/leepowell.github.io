71c8f1dc2cf2d44cde5701802ba0a3ca30ef3f3a
Lee Powell <81479565+leepowe@users.noreply.github.com>
2023-11-19 23:00:54 -0500

    replaced a lame project a more complex project

--- script.js
+++ script.js
@@ -4,31 +4,31 @@ const theme = localStorage.getItem('theme') || 'light';
 setTheme(theme);
 
 for (const themeDot of themeDots) {
-    themeDot.addEventListener('click', ()=> {
-        const mode = themeDot.dataset.mode;
-        setTheme(mode);
-    });
+	themeDot.addEventListener('click', () => {
+		const mode = themeDot.dataset.mode;
+		setTheme(mode);
+	});
 }
 
 function getStylesheetUrl(mode) {
-    return `css/${mode}.css`;
+	return `css/${mode}.css`;
 }
 
 function setTheme(mode) {
-    let themeStyle = document.getElementById('theme-style'),
-        defaultTheme = getStylesheetUrl('light');
+	let themeStyle = document.getElementById('theme-style'),
+		defaultTheme = getStylesheetUrl('light');
 
-    switch (mode) {
-        case 'light':
-        case 'blue':
-        case 'green':
-        case 'purple':
-            themeStyle.href = getStylesheetUrl(mode);
-            break;
-        default:
-            themeStyle.href = defaultTheme;
-            mode = 'light';
-            break;
-    }
-    localStorage.setItem('theme', mode);
+	switch (mode) {
+		case 'light':
+		case 'blue':
+		case 'green':
+		case 'purple':
+			themeStyle.href = getStylesheetUrl(mode);
+			break;
+		default:
+			themeStyle.href = defaultTheme;
+			mode = 'light';
+			break;
+	}
+	localStorage.setItem('theme', mode);
 }
